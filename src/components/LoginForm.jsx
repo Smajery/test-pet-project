31e278c29afb49a7cecf1797efa110a340db7570
Smajery <vasiliew.sasha@gmail.com>
2022-10-19 20:47:01 +0200

    add styles

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {useActions} from "../hooks/useActions";
 
 const LoginForm = () => {
     const {logIn} = useActions()
 
+    const [emailValue, setEmailValue] = useState('')
+    const [passwordValue, setPasswordValue] = useState('')
+
     return (
         <form className='login-form'>
-            <input/>
-            <input/>
+            <input
+                type='text'
+                placeholder='Введите свой email'
+                value={emailValue}
+                onChange={event => setEmailValue(event.target.value)}
+            />
+            <input
+                type='password'
+                placeholder='Введите свой пароль'
+                value={passwordValue}
+                onChange={event => setPasswordValue(event.target.value)}
+            />
             <button onClick={() => logIn()}>Войти</button>
         </form>
 
